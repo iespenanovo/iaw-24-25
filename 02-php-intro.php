e898224337efade7f5c74a83eac884302ba961dc
iespenanovo <correo.iaw@gmail.com>
2024-09-23 12:36:10 +0200

    ASIR 2

---
+++ 02-php-intro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PHP embebido en HTML</title>
+</head>
+<body>
+	<h1>PHP embebido en HTML</h1>
+	<?php 
+
+
+		echo "<h3>salida de la función phpinfo(): </h3>";
+		 phpinfo();
+
+	?>
+	
+
+</body>
+</html>
