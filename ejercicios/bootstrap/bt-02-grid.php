52ef74fa4ecdd0bbc5292ed570e42a8da6ac8cdb
iespenanovo <correo.iaw@gmail.com>
2024-11-29 13:10:48 +0100

    ASIR 2

---
+++ ejercicios/bootstrap/bt-02-grid.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap layout</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/bt-02-estilo.css">
+</head>
+<body>
+  <div class="container text-center">
+    <div class="row mb-3">
+      <div class="col">
+        <h1>Layout en Bootstrap</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1</div>
+      <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1</div>
+      <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1</div>
+      <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1</div>
+      <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1</div>
+      <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1</div>
+      <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1</div>
+      <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1</div>
+      <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1</div>
+      <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1</div>
+      <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1</div>
+      <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1</div>
+
+    </div>
+  </div>
+  
+</body>
+</html>
